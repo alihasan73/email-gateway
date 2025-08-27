3ed2f1253468dfb2f6bc087b71fb1f77efa38df4
Hasan Ali <69592065+alihasan73@users.noreply.github.com>
2025-08-27 21:13:06 +0700

    Mask sensitive SMTP configuration details
    Updated SMTP configuration to hide sensitive information.

--- src/config/smtp_config.js
+++ src/config/smtp_config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  host_smtp_5: "103.113.133.230",
-  port_smtp_5: 59277,
-  username_smtp_5: "ptkei",
-  password_smtp_5: "P@55w0rd",
-  command_smtp_5: null
+  host_smtp_5: "*******",
+  port_smtp_5: "*******",
+  username_smtp_5: "******",
+  password_smtp_5: "*******",
+  command_smtp_5: "****"
 };
