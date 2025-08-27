437aabe6b1505990d1824d9a9e8e859877565bbe
PTKEI <hasansanad73@gmail.com>
2025-08-27 12:56:41 +0700

    ignore script dan data

--- scripts/scheduler.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-const scheduler = require('../src/services/scheduler.service');
-const intervalArg = process.env.SCHED_INTERVAL_MS || process.argv[2];
-const intervalMs = intervalArg ? parseInt(intervalArg,10) : undefined;
-const runner = scheduler.start({ intervalMs });
-process.on('SIGINT', ()=>{ console.log('stop'); runner.stop(); process.exit(0); });
